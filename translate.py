f0907249ab87434f79d9a506ba41733858d1f81d
eaminhu <eaminhu@163.com>
2023-09-15 12:57:41 +0800

    Add files via upload

--- translate.py
+++ translate.py
@@ -1,9 +1,9 @@
 from deep_translator import GoogleTranslator
 
-def translate_file(input_file, output_file, target_lang):
+def translate_file(input_file, output_file):
    with open(input_file, 'r', encoding='utf-8') as source_file:
        source_text = source_file.read()
-       translated_text = GoogleTranslator(source='zh-CN', target=target_lang).translate(source_text)
+       translated_text = GoogleTranslator(source='zh-CN', target='en').translate(source_text)
    with open(output_file, 'w', encoding='utf-8') as target_file:
        target_file.write(translated_text)
 
