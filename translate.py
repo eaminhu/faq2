4d3e3c56220a8fca3177981de4fa054f734a222f
eaminhu <eaminhu@163.com>
2023-09-15 14:11:58 +0800

    Update translate.py

--- translate.py
+++ translate.py
@@ -3,7 +3,7 @@ from deep_translator import GoogleTranslator
 def translate_file(input_file, output_file):
    with open(input_file, 'r', encoding='utf-8') as source_file:
        source_text = source_file.read()
-       translated_text = GoogleTranslator(source='zh-CN', target='en').translate(source_text)
+       translated_text = GoogleTranslator(source='zh-CN', target='ru').translate(source_text)
    with open(output_file, 'w', encoding='utf-8') as target_file:
        target_file.write(translated_text)
 
