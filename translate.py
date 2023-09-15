6bc3426dc4d26cd4986f7ab7eb4d71e60648c125
eaminhu <eaminhu@163.com>
2023-09-15 14:20:39 +0800

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,21 +1,20 @@
-import re
 from deep_translator import GoogleTranslator
+import mistune  # 导入Markdown解析库
 
-def translate_markdown_file(input_file, output_file):
+def extract_text_from_markdown(input_file):
     with open(input_file, 'r', encoding='utf-8') as source_file:
         source_text = source_file.read()
+        # 使用mistune解析Markdown，提取文本内容
+        markdown_parser = mistune.create()
+        text_content = markdown_parser(source_text)
+    return text_content
 
-    # 使用正则表达式提取Markdown中的文本内容
-    text_content = re.sub(r'<.*?>', '', source_text)  # 移除所有标签
-
-    # 使用Google翻译器翻译文本
+def translate_file(input_file, output_file):
+    text_content = extract_text_from_markdown(input_file)
+    # 将提取的文本内容翻译为俄文
     translated_text = GoogleTranslator(source='zh-CN', target='ru').translate(text_content)
-
-    # 替换原始Markdown文件中的文本内容
-    translated_source = re.sub(re.escape(text_content), translated_text, source_text)
-
     with open(output_file, 'w', encoding='utf-8') as target_file:
-        target_file.write(translated_source)
+        target_file.write(translated_text)
 
 if __name__ == "__main__":
     import sys
@@ -24,4 +23,4 @@ if __name__ == "__main__":
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
-        translate_markdown_file(input_file, output_file)
+        translate_file(input_file, output_file)
